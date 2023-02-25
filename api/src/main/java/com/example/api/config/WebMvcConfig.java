6eaace4abeb559d70128e7e262e805b11704204a
kentny <kentaro.terasaki@gmail.com>
2023-02-25 16:49:34 +0900

    Simplify the CORS setting.

--- api/src/main/java/com/example/api/config/WebMvcConfig.java
+++ api/src/main/java/com/example/api/config/WebMvcConfig.java
@@ -7,12 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Value("${cors.allowed-origin}")
-    private String allowedOrigin;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(this.allowedOrigin);
+                .allowedOrigins("*");
     }
 }
